8bce05e1d4a6f9a94c511703838145e84c8272a1
Dmitry <ingredibled000777@gmail.com>
2021-07-25 22:04:31 +0300

    Обновил input

--- VueJS/test-project-vue-client/src/components/quest_6.vue
+++ VueJS/test-project-vue-client/src/components/quest_6.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
-    <h1>{{ item.question }}</h1>
-    <input type="text"
-           v-on:change="checking($event.target)">
-  </div>
+  <el-container>
+    <el-main>
+      <el-row>
+        <el-col>
+          <h1>{{ item.question }}</h1>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col>
+          <el-input
+              placeholder="Please input"
+              v-model="input"
+              @change="checking"
+              clearable>
+          </el-input>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
@@ -11,13 +26,14 @@ export default {
   name: "quest_6",
   data() {
     return {
-      rightAnswer: this.item.responses[0].text
+      rightAnswer: this.item.responses[0].text,
+      input: '',
     }
   },
   props: ['item', 'index', 'userAnswers'],
   methods: {
     checking: function (obj) {
-      if (obj.value.toLowerCase() === this.rightAnswer.toLowerCase()) {
+      if (obj.toLowerCase() === this.rightAnswer.toLowerCase()) {
         this.userAnswers[this.index] = 1;
       }
       else {
