cf185a22e7a5be56b5524ba89de95af4c8f28034
Dmitry <ingredibled000777@gmail.com>
2021-07-25 17:24:44 +0300

    Обновил радио

--- VueJS/test-project-vue-client/src/components/quest_1.vue
+++ VueJS/test-project-vue-client/src/components/quest_1.vue
@@ -1,31 +1,46 @@
 <template>
-  <div>
-    <h1>{{ item.question }}</h1>
-    <ol>
-      <li v-for="response in item.responses" :key="response.text">
-        <label>
-          <input type="radio"
-                 v-bind:value="response.correct"
-                 v-bind:name="index"
-                 v-on:click="checking($event.target)">
-          {{ response.text }}
-        </label>
-      </li>
-    </ol>
-  </div>
+  <el-container>
+    <el-main>
+      <el-row>
+        <el-col>
+          <h1>{{ item.question }}</h1>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col>
+          <el-radio-group
+              id="main"
+              v-model="radio" @change="checking">
+            <el-radio
+                id="sub"
+                v-for="response in item.responses" :key="response.text"
+                :label="!!response.correct">
+              {{ response.text }}
+            </el-radio>
+          </el-radio-group>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
 export default {
   name: "quest_1",
   props: ['item', 'index', 'userAnswers'],
+  data() {
+    return {
+      radio: 0,
+    };
+  },
   methods: {
-    checking: function (obj) {
+    checking: function (e) {
       let countOfRightAnswers = this.item.responses.filter((elem) => elem.correct === true).length;
 
       let score = 1 / countOfRightAnswers;
 
-      if (obj.value.toLowerCase() === 'true'.toLowerCase()) {
+      if (e) {
         this.userAnswers[this.index] = score;
       }
       else {
